be0a82b7d9a60b7bf7d18ed7504ebd944449771c
ChayawatS <mjpoak@gmail.com>
2018-04-02 22:20:58 +0700

    Initial Commit

---
+++ SevenShake_ios/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SevenShake_ios
+//
+//  Created by chayarak on 2/4/2561 BE.
+//  Copyright © 2561 chayarak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

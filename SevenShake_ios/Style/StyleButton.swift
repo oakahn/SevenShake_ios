9ce98659075a4b8db5287fade048e4d78b5174a0
ChayawatS <mjpoak@gmail.com>
2018-04-04 23:03:35 +0700

    Add Button

---
+++ SevenShake_ios/Style/StyleButton.swift
@@ -0,0 +1,21 @@
+//
+//  StyleButton.swift
+//  SevenShake_ios
+//
+//  Created by chayarak on 4/4/2561 BE.
+//  Copyright © 2561 chayarak. All rights reserved.
+//
+
+import UIKit
+
+class StyleButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
